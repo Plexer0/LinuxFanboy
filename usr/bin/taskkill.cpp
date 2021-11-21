4d1ed434ff3e3c774abc4b82c7a6e38e0e6d9228
Plexer <plexer0@protonmail.com>
2021-11-22 08:15:20 +1100

    Fix grammar for taskkill.cpp

--- usr/bin/taskkill.cpp
+++ usr/bin/taskkill.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
 int main()
 {
-	std::cout << "You do realise there is a command for pkill and xkill?\n";
+	std::cout << "You do realise there is a command called pkill and xkill?\n";
 }
