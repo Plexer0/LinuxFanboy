d0a7906a74559a73153b8b74d60680e5e484b06d
Plexer <plexer0@protonmail.com>
2021-11-26 08:17:10 +1100

    Add new commands and update message in slui.cpp

---
+++ usr/bin/fdisk.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+int main()
+{
+	std::cout << "fdisk, diskpart, diskmgnt... JUST FIND A UNIVERSAL TOOL!\n";
+}
+
--- usr/bin/slui.cpp
+++ usr/bin/slui.cpp
@@ -2,6 +2,6 @@
 int main()
 {
 	std::cout << "zsh: command not found: slui\n";
-	std::cout << "You should be happy. Linux is FOSS\n";
+	std::cout << "You should be happy! Linux is FOSS.\n";
 	std::cout << "And guess what, Windows isn't!\n";
 }
