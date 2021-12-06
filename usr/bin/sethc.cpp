23fb57cbc8ca58db3bd311d82dd5e0c674b97878
Plexer <plexer0@protonmail.com>
2021-12-07 08:24:28 +1100

    Copy utilman.cpp as sethc.cpp

---
+++ usr/bin/sethc.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+int main()
+{
+	std::cout << "There is no secret backdoor in Linux! All the code is FOSS.\n";
+	std::cout << "Windows on the other hand, is secret squirrel.\n";
+}
