4dc9bf9c50cf22f78977feafb388788596494bb1
Plexer <plexer0@protonmail.com>
2021-12-26 15:05:09 +1100

    Fix formatting issue with runas.cpp

--- usr/bin/runas.cpp
+++ usr/bin/runas.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 int main()
 {
-	char user = system("echo $USER");
-	std::cout << user << "is wasting their time running silly Windows commands. This incident will be reported to Tux.\n";
+	char user = system("echo -n $USER");
+	std::cout << user << " is wasting their time running silly Windows commands. This incident will be reported to Tux.\n";
 }
