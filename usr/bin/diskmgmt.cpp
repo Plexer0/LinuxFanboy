61b0713f454d696732277e72e2e4ccb1914915c6
Plexer <plexer0@protonmail.com>
2022-01-03 15:51:56 +1100

    Rename fdisk.cpp to diskmgmt.cpp [🤦]

---
+++ usr/bin/diskmgmt.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+int main()
+{
+	std::cout << "diskmgnt, diskpart, chkdsk... JUST FIND A UNIVERSAL TOOL!\n";
+}
+
--- usr/bin/fdisk.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-int main()
-{
-	std::cout << "fdisk, diskpart, diskmgnt... JUST FIND A UNIVERSAL TOOL!\n";
-}
-
