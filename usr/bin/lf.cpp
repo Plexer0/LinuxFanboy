de4aceb8ee9bc258fc41c476134b9e9cc05727f0
Plexer <plexer0@protonmail.com>
2022-01-04 16:42:04 +1100

    Add welcome/intro program, lf.cpp

---
+++ usr/bin/lf.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+int main()
+{
+// Announce License
+
+	std::cout << "Linux Fanboy V2.0, Copyright (C) 2022 Plexer\n";
+	std::cout << "LinuxFanboy comes with ABSOLUTELY NO WARRANTY; for details type `show w'.\n";
+	std::cout << "This is free software, and you are welcome to redistribute it\n";
+	std::cout << "under certain conditions; type `show c' for details.\n";
+	std::cout << "~\n";
+
+// Start main function
+
+	std::cout << "G'day and welcome to LinuxFanboy!\n";
+	std::cout << "This package has added 'Windows programs' to your /usr/bin folder.\n";
+	std::cout << "These won't execute the real thing, but you will recieve a funny message that a steriotypical, hardcore Linux fan would respond with.\n";
+	std::cout << "\n";
+	std::cout << "This programs' purpose isn't to take a swing at Microsoft or the Linux community in any way. Everything here is just for fun and laughs! :D\n";
+	std::cout << "If you would like to contribute, check out the GitLab project! https://gitlab.com/Plexer0/LinuxFanboy\n";
+	std::cout << "\n";
+	std::cout << "To get started, why don't you run the old-school win command?\n";
+}
