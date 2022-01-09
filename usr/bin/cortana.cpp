5b6b6f54579c6b66c9bc42c79ecae7b2312bf848
Plexer <plexer0@protonmail.com>
2022-01-09 16:45:49 +1100

    Add new programs: cortana.cpp and winver.cpp

---
+++ usr/bin/cortana.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+int main()
+{
+	std::cout << "EEK! No spyware is preinstalled on Linux!\n";
+	std::cout << "Microshaft should learn a thing or two about that!\n";
+}
---
+++ usr/bin/winver.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+int main()
+{
+	system("neofetch");
+	std::cout << "Now THATS a 'winver'! Look how low my memory usage is!\n";
+	std::cout << "Did I mention it's less bloated than Winblows?\n";
+}
