14d70651a16574b29b984c614c3b40860083e2b0
Plexer <plexer0@protonmail.com>
2022-01-13 20:35:52 +1100

    Rewrite program gpupdate.cpp

--- usr/bin/gpupdate.cpp
+++ usr/bin/gpupdate.cpp
@@ -1,5 +1,18 @@
 #include <iostream>
 int main()
 {
-	std::cout << "FATAL ERROR! Please execute gpedit for details.\n";
+	std::cout << "Updating policy...\n";
+	system("sleep 5");
+	std::cout << "Set DataCollection == 'Enabled'\n";
+	system("sleep 1");
+	std::cout << "Set DefaultBrowser == 'Edge'\n";
+	system("sleep 1");
+	std::cout << "Set UpdateMode == 'Forced'\n";
+	system("sleep 1");
+	std::cout << "Set UserVoice == 'XD'\n";
+	std::cout << "Computer policy updated successfully.\n";
+	std::cout << "\n";
+	std::cout << "See how annoying Windows can be?\n";
+	std::cout << "For an annyoing-free experience, try GNU\\Linux.\n";
+
 }
