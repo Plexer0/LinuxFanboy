cd83ec5d1bd9fe88ccb02c92f48c2b735dcf730e
Plexer <plexer0@protonmail.com>
2022-01-13 13:30:51 +1100

    Add new commands color.cpp and winlogon.cpp

---
+++ usr/bin/color.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+int main()
+{
+	std::cout << "No need to muck around in the silly Registry Editor anymore!\n";
+	std::cout << "Just configure your terminal software and you're away!\n";
+}
+
---
+++ usr/bin/winlogon.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+int main()
+{
+	std::cout << "Linux lets you choose from a wide varity of display managers.\n";
+	std::cout << "Windows gives you no choices, but Linux does.\n";
+}
