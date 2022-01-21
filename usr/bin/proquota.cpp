cfa5797f77033a3a215f6e6738f2201300617d49
Plexer <plexer0@protonmail.com>
2022-01-22 10:03:38 +1100

    Add new programs proquota.cpp and spaceman.cpp

---
+++ usr/bin/proquota.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+int main()
+{
+	std::cout << "Just setup separate partitions for your /home folder and you're away.\n";
+}
---
+++ usr/bin/spaceman.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+int main()
+{
+	std::cout << "Dude, there is already a program called proquota.\n";
+	std::cout << "Why are you bloating up my system by multiplying programs!!!\n";
+}
