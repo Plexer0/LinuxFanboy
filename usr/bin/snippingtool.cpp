9f12b4fd18f3e5e988882e7c1de1706c5da775ab
Plexer <plexer0@protonmail.com>
2022-01-21 19:36:00 +1100

    Add new program snippingtool.cpp

---
+++ usr/bin/snippingtool.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+int main()
+{
+	std::cout << "Spectacle has many more features than Snipping Tool.\n";
+	std::cout << "And get this, Spectacle is open-source!\n";
+	std::cout << "And also get this, Snipping Tool is proprietary!\n";
+}
