9b83268ead61f9f71dd417702cc6e1769afaeece
Plexer <plexer0@protonmail.com>
2022-02-12 21:04:07 +1100

    Add new program androidisthebest.cpp

---
+++ usr/bin/androidisthebest.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+int main()
+{
+	std::cout << "ANDROID IS NOT A REAL DISTRIBUTION OF LINUX!\n";
+	std::cout << "Sure it has some similarities, but you can't run neofetch so it sucks.\n";
